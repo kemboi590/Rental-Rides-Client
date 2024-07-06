6c807d7d6f85757d7ae080b8b158b8803f929e8d
kemboi590 <bkemboi590@gmail.com>
2024-07-06 18:10:32 +0300

    small fix on image service and ul

--- src/pages/landingPage/Services.tsx
+++ src/pages/landingPage/Services.tsx
@@ -3,21 +3,21 @@ import { Check } from 'lucide-react';
 const Services = () => {
     return (
 
-        <div className="container mx-auto py-10 grid grid-cols-1 gap-8 md:gap-12 lg:grid-cols-2">
-            <div className="flex justify-center items-center">
-                <img src={serviceimg} alt="services" className="w-96" />
+        <div className="container mx-auto py-10 grid grid-cols-1 gap-8 md:gap-10 lg:grid-cols-2">
+            <div className="flex justify-center items-center w-[80%] lg:w-[60%] mx-auto">
+                <img src={serviceimg} alt="services" className="w-full" />
             </div>
             <div>
                 <h2 className="font-bold text-center lg:text-start text-xl md:text-4xl text-[#FF914D]">Our Services</h2>
                 
-                <div className="flex flex-col justify-center items-center lg:flex-none lg:justify-start lg:items-start">
+                <div className="flex flex-col justify-center lg:flex-none lg:justify-start lg:items-start">
                 <p className="text-lg mt-5 px-6 lg:px-0">
                     We offer a wide range of services to our customers. <br />
                      Our services include:
                 </p>
 
      {/* Ul */}
-                <ul className="mt-5 list-none">
+                <ul className="mt-5 list-none mx-5">
                     <li className="flex gap-2">
                         <Check size={20} color='green' />
                         Car Rental
