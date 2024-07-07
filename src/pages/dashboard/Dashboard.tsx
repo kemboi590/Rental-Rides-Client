0bc6b44e6ecbd740808ee9cbedee5673ee04ca83
kemboi590 <bkemboi590@gmail.com>
2024-07-08 00:43:09 +0300

    small edit

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -7,12 +7,12 @@ const Dashboard = () => {
     <div>
       <Navbar />
 
-      <div className="flex lg:flex-row ">
-        <aside className="w-full lg:w-[20%]">
+      <div className="flex lg:flex-row gap-4">
+        <aside className="w-0 lg:w-[23%]">
           <Drawer />
         </aside>
 
-        <main className="w-full lg:w-[80%] bg-webcolor">
+        <main className="w-full bg-webcolor">
           <Outlet />
         </main>
       </div>
