5b7bb2fc3062cad78e0528073ba14e1705cd0829
kemboi590 <bkemboi590@gmail.com>
2024-07-14 20:44:12 +0300

    availability to dashboard when we have a user

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
 import Drawer from "./aside/Drawer";
+import { useSelector } from 'react-redux';
+import { RootState } from "../../app/store";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const user = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    if (!user || !user.user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
   return (
     <div className="h-screen flex flex-col">
       <Navbar />
@@ -21,6 +33,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-
-
--- src/pages/landingPage/Hero.tsx
+++ src/pages/landingPage/Hero.tsx
@@ -1,11 +1,11 @@
+import { Link } from 'react-router-dom';
 import { RootState } from '../../app/store';
 import bgrides from '../../assets/images/bgrides.jpeg'
 import { useSelector } from 'react-redux';
 
 const Hero = () => {
     const user = useSelector((state: RootState) => state.user);
-
-    console.log("from Hero", user.user);
+    // console.log("from Hero", user.user);
 
     const name = user.user?.name;
     return (
@@ -29,7 +29,8 @@ const Hero = () => {
                         Rent a car from our extensive range of vehicles. We have every type of car available to meet your needs. <br />
                         We offer affordable Pricing to our Customers
                     </p>
-                    <button className="btn bg-webcolor text-text-light hover:text-black border-none">Book a Ride</button>
+                    <Link to="/dashboard/vehicles" className="btn bg-webcolor text-text-light hover:text-black border-none">Book a Ride</Link>
+                    {/* <button className="btn bg-webcolor text-text-light hover:text-black border-none">Book a Ride</button> */}
                 </div>
             </div>
         </div>
