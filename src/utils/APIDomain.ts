f862184674ac68f048832ac399d8faab714696cb
kemboi590 <bkemboi590@gmail.com>
2024-07-14 13:51:51 +0300

    back to deployed

--- src/utils/APIDomain.ts
+++ src/utils/APIDomain.ts
@@ -1,3 +1,3 @@
 // local host
-export const APIDomain = 'http://localhost:8080'
-// export const APIDomain = 'https://rental-rides-api.azurewebsites.net'
+// export const APIDomain = 'http://localhost:8080'
+export const APIDomain = 'https://rental-rides-api.azurewebsites.net'
